0b2dd75ff81f768866a595a113e9977b74cf254d
Ben Tucker <benjitucker@mac.com>
2010-11-20 01:23:17 +0100

    Hacks for building MTD-UBI inside UML. NOT FOR GENERAL CONSUMPTION.

--- arch/um/include/asm/io.h
+++ arch/um/include/asm/io.h
@@ -33,25 +33,61 @@ static inline void * phys_to_virt(unsigned long address)
  */
 #define xlate_dev_kmem_ptr(p)	p
 
+extern inline u8 readb(const volatile void __iomem *addr)
+{
+	printk("readb %p\n", addr);
+	return 0;
+}
+extern inline u16 readw(const volatile void __iomem *addr)
+{
+	printk("readw %p\n", addr);
+	return 0;
+}
+static inline u32 readl(const volatile void __iomem *addr)
+{
+	printk("readl %p\n", addr);
+	return 0;
+}
+static inline u64 readq(const volatile void __iomem *addr)
+{
+	printk("readq %p\n", addr);
+	return 0;
+}
 static inline void writeb(unsigned char b, volatile void __iomem *addr)
 {
+	printk("writeb %p=0x%02x\n", addr, b);
 	*(volatile unsigned char __force *) addr = b;
 }
 static inline void writew(unsigned short b, volatile void __iomem *addr)
 {
+	printk("writew %p=0x%04x\n", addr, b);
 	*(volatile unsigned short __force *) addr = b;
 }
 static inline void writel(unsigned int b, volatile void __iomem *addr)
 {
+	printk("writel %p=0x%08x\n", addr, b);
 	*(volatile unsigned int __force *) addr = b;
 }
 static inline void writeq(unsigned int b, volatile void __iomem *addr)
 {
+	printk("writeq %p=0x%08x\n", addr, b);
 	*(volatile unsigned long long __force *) addr = b;
 }
 #define __raw_writeb writeb
 #define __raw_writew writew
 #define __raw_writel writel
 #define __raw_writeq writeq
+ 
+#define readb(c)		({ u8  __v = readb_relaxed(c); __iormb(); __v; })
+#define readw(c)		({ u16 __v = readw_relaxed(c); __iormb(); __v; })
+#define readl(c)		({ u32 __v = readl_relaxed(c); __iormb(); __v; })
+
+#define readb_relaxed(addr) readb(addr)
+#define readw_relaxed(addr) readw(addr)
+#define readl_relaxed(addr) readl(addr)
+#define readq_relaxed(addr) readq(addr)
+
+#define __iormb()		do { } while (0)
+#define __iowmb()		do { } while (0)
 
 #endif

f74026cdfe1168dca65662960754afad1e66544e
Ben Tucker <benjitucker@mac.com>
2010-11-20 01:23:17 +0100

    Hacks for building MTD-UBI inside UML. NOT FOR GENERAL CONSUMPTION.

--- arch/um/include/asm/delay.h
+++ arch/um/include/asm/delay.h
@@ -14,6 +14,8 @@ extern void __delay(unsigned long loops);
 
 #define ndelay(n) ((__builtin_constant_p(n) && (n) > 20000) ? \
 	__bad_ndelay() : __ndelay(n))
+//extern void __unimplemented_ndelay(void);
+//#define ndelay(n) __unimplemented_ndelay()
 #define ndelay(n)		do { } while (0)
 
 #endif
